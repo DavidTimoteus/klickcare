ee51f5592e1de8e315175f5eb975661dddd12f31
DavidTimoteus <2131710012@student.polinema.ac.id>
2024-06-13 23:47:18 +0700

    Konfigurasi awal

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,13 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tbl_users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 50);
+            $table->string('noId', 30)->unique();
+            $table->string('email', 50)->unique();
+            $table->string('password', 255);
+            $table->enum('gender', ['Laki-Laki', 'Perempuan']);
+            $table->date('birthDate');
+            $table->string('address', 100);
+            $table->string('phoneNumber', 255);
+            $table->unsignedBigInteger('role')->default(1);
+            $table->unsignedBigInteger('create_id')->nullable();
+            $table->timestamp('creation_time')->nullable();
+            $table->unsignedBigInteger('update_id')->nullable();
+            $table->timestamp('update_time')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +35,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tbl_users');
     }
 };
